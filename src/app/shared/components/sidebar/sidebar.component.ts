f92e519102cb240470a85281993ef27311040b1e
zFranco <francoheflo@gmail.com>
2025-06-11 22:58:27 -0500

    feat: add temporarily logout button

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { Avatar } from '../avatar/avatar.component';
 import { SidebarItem } from './sidebar-item/sidebar-item.component';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  imports: [PanelMenuModule, SidebarItem, Avatar],
+  imports: [PanelMenuModule, SidebarItem, Avatar, ButtonModule],
 })
-export class Sidebar {}
+export class Sidebar {
+  private router = inject(Router);
+
+  // Should abstract this in a service
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
