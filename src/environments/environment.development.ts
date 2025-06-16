25b2c32b493f038bbf100b5148c9da281ad17983
zFranco <francoheflo@gmail.com>
2025-06-15 19:12:09 -0500

    feat: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://trackly-backend-of25.onrender.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://trackly-backend-of25.onrender.com',
+};
